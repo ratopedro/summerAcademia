07705dade751bb1a94daff9d212465a0cab49c40
Gustavo Pires <gmpp@av.it.pt>
2013-07-12 17:01:33 +0100

    One more client

---
+++ androidClients/RedesDivertidasMapaVazio/src/pt/it/av/atnog/map/LocationBehavior.java
@@ -0,0 +1,58 @@
+package pt.it.av.atnog.map;
+
+import pt.it.av.atnog.funNet.remoteCDB.RCDB;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class LocationBehavior {
+	private Location lastLocation;
+	private RCDB db;
+	private Criteria criteria;
+
+	LocationBehavior(LocationManager locationManager, RCDB db) {
+		this.db = db;
+
+		criteria = new Criteria();
+		criteria.setAltitudeRequired(false);
+		criteria.setCostAllowed(false);
+		criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		
+		lastLocation = new Location(locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER));
+		db.set(lastLocation.getLatitude(), lastLocation.getLongitude());
+		
+		// Register the listener with the Location Manager to receive location updates
+		locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 1000, 0, locationListener);
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3000, 0, locationListener);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+	}
+	
+	private void changeLocation(Location location)
+	{
+		if(lastLocation.getAccuracy() < location.getAccuracy() || location.getTime() - lastLocation.getTime() > 10000)
+		{
+			this.db.set(location.getLatitude(), location.getLongitude());
+			lastLocation = location;
+		}
+	}
+	
+	// Define a listener that responds to location updates
+	LocationListener locationListener = new LocationListener() {
+		public void onLocationChanged(Location location) {
+			changeLocation(location);
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+
+		public void onProviderEnabled(String provider) {
+		}
+
+		public void onProviderDisabled(String provider) {
+		}
+	};
+}
---
+++ androidClients/RedesDivertidasMapaVazio/src/pt/it/av/atnog/map/MapsFragment.java
@@ -0,0 +1,86 @@
+package pt.it.av.atnog.map;
+
+import java.util.Timer;
+
+import pt.it.av.atnog.R;
+import pt.it.av.atnog.TTMethod;
+import pt.it.av.atnog.funNet.remoteCDB.RCDB;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import android.content.Context;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapsFragment extends Fragment {
+	private GoogleMap map;
+	private RCDB db;
+	private LocationBehavior locationBehavior;
+	private Handler markerHandler;
+	
+	private Timer timer;
+	
+	private String nome;
+	private int icon;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Obter argumentos
+		Bundle bun = getArguments();
+		
+		// Inicializar variáveis com os argumentos
+		this.nome = bun.getString("name");
+		this.icon = bun.getInt("icon");
+		
+		return inflater.inflate(R.layout.activity_map, container, false);
+	}
+	
+	@Override    
+	public void onStart() {
+        super.onStart();
+
+        if(map == null)
+        {
+        	// Obter o objecto referente ao mapa;
+        	map = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        	
+        	// Colocar a vista do mapa sobre Portugal
+        	map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40, -8), 6.0f));
+        	
+        	// Retirar os butões de zoom da interface
+			map.getUiSettings().setZoomControlsEnabled(false);
+			
+			/**
+			 * Define o tipo de mapa, possíveis valores:
+			 * 
+			 * Normal - Typical road map. Roads, some man-made features, and important natural features such as rivers are shown. Road and feature labels are also visible.
+			 * Hybrid - Satellite photograph data with road maps added. Road and feature labels are also visible.
+			 * Satellite - Satellite photograph data. Road and feature labels are not visible.
+			 * Terrain - Topographic data. The map includes colors, contour lines and labels, and perspective shading. Some roads and labels are also visible.
+			 * None - No tiles. The map will be rendered as an empty grid with no tiles loaded.
+			 */
+			map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+			
+        	Options.EEInicialization(map);
+			
+			// Location database
+			db = new RCDB(nome);
+			locationBehavior = new LocationBehavior((LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE), db);
+
+			markerHandler = new HandlerMapDisplay(map, db, nome, icon);
+			
+			timer = new Timer();
+			TTMethod method = new TTMethod(markerHandler); 
+			timer.schedule(method, 5000, 5000);
+        }
+	}
+}
